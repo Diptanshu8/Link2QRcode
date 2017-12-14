ef56863cb282a575640515f0d9afb3d1b2d2a1c3
Diptanshu8 <diptanshuj@gmail.com>
2017-12-14 11:06:52 +0000

    Added argparse params to save the output with a name and location input from the user. WIP: How to write the generated image at user specified location and name.

--- L2QR.py
+++ L2QR.py
@@ -3,38 +3,52 @@ import argparse
 from PIL import ImageColor
 
 def parameter_handler(args):
-    global back_color,fill_color,link
+    global back_color,fill_color,link, location, output_name
+    color_choices = ImageColor.colormap.keys()
     #setting the default back_color for QR code and checking the user specified param for validity.
     back_color = "white"
     if args.background_color: 
-	    back_color = args.background_color
-	    if args.background_color not in color_choices :
-		    print "Error: invalid color choice for background color.\nThe available choices are \n"+ "\t".join(color_choices)
-		    exit()
+        back_color = args.background_color
+        if args.background_color not in color_choices :
+            print "Error: invalid color choice for background color.\nThe available choices are \n"+ "\t".join(color_choices)
+            exit()
 
     #setting the default fill_color for QR code and checking the user specified param for validity.
     fill_color = "black"
     if args.fill_color: 
-	    fill_color = args.fill_color
-	    if args.fill_color not in color_choices :
-		    print "Error: invalid color choice for fill color.\nThe available choices are \n"+ "\t".join(color_choices)
-		    exit()
+        fill_color = args.fill_color
+        if args.fill_color not in color_choices :
+            print "Error: invalid color choice for fill color.\nThe available choices are \n"+ "\t".join(color_choices)
+            exit()
+
+    #checking if the output image needs to be saved.
+    #if QR code has to be saved then under what name, format and location.
+    if args.save:
+        if not args.location:
+            print "Error: The user didn't specify the location to save the QR code."
+            exit()
+        else:
+            location = args.location
+        if not args.name:
+            print "Error: The user didn't specify the name by which the the QR code will be saved."
+            exit()
+        else:
+            output_name = args.name
 
     #retrieving the URL from argparse
     link = args.url
 
 
 parser = argparse.ArgumentParser()
-color_choices = ImageColor.colormap.keys()
 parser.add_argument("url", help = "The URL for which the user wants to generate the QR code.")
+
 parser.add_argument("-bc","--background_color", help = "The background color in the generated QR code.")
 parser.add_argument("-fc","--fill_color",help = "The URL for which the user wants to generate the QR code.")
-args = parser.parse_args()
 
-#setting default values to the 
-back_color = "white"
-fill_color = "black"
-link=""
+parser.add_argument("--save",action = "store_true", help = "Bool flag if the user wants to save the generated QR code.")
+parser.add_argument("--location",help = "The location(local) to save the QR code.")
+parser.add_argument("--name",help = "The name by which the QR code be saved.")
+args = parser.parse_args()
 
 #parsing the parameters set by the user
 parameter_handler(args)

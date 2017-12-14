666af1c19cec846846d43c680381872f1a1d5c74
Diptanshu8 <diptanshuj@gmail.com>
2017-12-14 09:48:20 +0000

    Added the parameter parser function to parse the parameters. Need to think of a way so that the global variables need not be defined initially.

--- L2QR.py
+++ L2QR.py
@@ -2,28 +2,47 @@ import qrcode
 import argparse
 from PIL import ImageColor
 
+def parameter_handler(args):
+    global back_color,fill_color,link
+    #setting the default back_color for QR code and checking the user specified param for validity.
+    back_color = "white"
+    if args.background_color: 
+	    back_color = args.background_color
+	    if args.background_color not in color_choices :
+		    print "Error: invalid color choice for background color.\nThe available choices are \n"+ "\t".join(color_choices)
+		    exit()
+
+    #setting the default fill_color for QR code and checking the user specified param for validity.
+    fill_color = "black"
+    if args.fill_color: 
+	    fill_color = args.fill_color
+	    if args.fill_color not in color_choices :
+		    print "Error: invalid color choice for fill color.\nThe available choices are \n"+ "\t".join(color_choices)
+		    exit()
+
+    #retrieving the URL from argparse
+    link = args.url
+
+
 parser = argparse.ArgumentParser()
 color_choices = ImageColor.colormap.keys()
 parser.add_argument("url", help = "The URL for which the user wants to generate the QR code.")
-parser.add_argument("--background_color", help = "The background color in the generated QR code.")
-parser.add_argument("--fill_color",help = "The URL for which the user wants to generate the QR code.")
+parser.add_argument("-bc","--background_color", help = "The background color in the generated QR code.")
+parser.add_argument("-fc","--fill_color",help = "The URL for which the user wants to generate the QR code.")
 args = parser.parse_args()
 
-if args.background_color and args.background_color not in color_choices :
-	print "Error: invalid color choice for background color.\nThe available choices are \n"+ "\t".join(color_choices)
-if args.fill_color and args.fill_color not in color_choices :
-	print "Error: invalid color choice for fill color.\nThe available choices are \n"+ "\t".join(color_choices)
-link = args.url
-print link
+#setting default values to the 
+back_color = "white"
+fill_color = "black"
+link=""
 
+#parsing the parameters set by the user
+parameter_handler(args)
 
-"""
 #generating the QR code according to doc at https://github.com/lincolnloop/python-qrcode
 qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=10, border=4,)
 qr.add_data(link)
 qr.make(fit=True)
 
-img = qr.make_image(fill_color = "black", back_color = "white")
-img.show()
-
-"""
+img = qr.make_image(fill_color = fill_color, back_color = back_color)
+#img.show()
